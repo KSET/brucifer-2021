755e403fff7deb884b2ee4b127225ea7c62c5c14
Josip Igrec <josip@igr.ec>
2022-09-14 15:28:04 +0200

    Move main config to function

--- main.go
+++ main.go
@@ -43,6 +43,35 @@ var views embed.FS
 const defaultPort = 3000
 const defaultHost = "0.0.0.0"
 
+type appConfig struct {
+	Port int
+	Host string
+}
+
+func loadConfig() appConfig {
+	envPort, err := strconv.ParseInt(os.Getenv("PORT"), 0, 32)
+	if err != nil || envPort == 0 {
+		envPort = defaultPort
+	}
+
+	envHost := os.Getenv("HOST")
+	if envHost == "" {
+		envHost = defaultHost
+	}
+
+	var port int
+	var host string
+
+	flag.IntVarP(&port, "port", "p", int(envPort), "Set the port on which the server will run")
+	flag.StringVarP(&host, "host", "h", envHost, "Set the host to which the server will bind")
+	flag.Parse()
+
+	return appConfig{
+		Port: port,
+		Host: host,
+	}
+}
+
 func cspConfig() string {
 	cspEntries := map[string][]string{
 		"default-src": {
@@ -88,25 +117,9 @@ func main() {
 		log.Println("Error loading .env file: ", err)
 	}
 
-	envPort, err := strconv.ParseInt(os.Getenv("PORT"), 0, 32)
-	if err != nil || envPort == 0 {
-		envPort = defaultPort
-	}
+	conf := loadConfig()
 
-	envHost := os.Getenv("HOST")
-	if envHost == "" {
-		envHost = defaultHost
-	}
-
-	var port int
-	var host string
-
-	flag.IntVarP(&port, "port", "p", int(envPort), "Set the port on which the server will run")
-	flag.StringVarP(&host, "host", "h", envHost, "Set the host to which the server will bind")
-
-	flag.Parse()
-
-	_, err = async.Async().RunInParallel(
+	_, err := async.Async().RunInParallel(
 		func() (interface{}, error) {
 			return nil, hash.HashProvider().CalibrateMemoryParam(time.Second)
 		},
@@ -226,5 +239,5 @@ func main() {
 
 	routes.RegisterRoutes(app)
 
-	log.Fatal(app.Listen(fmt.Sprintf("%s:%d", host, port)))
+	log.Fatal(app.Listen(fmt.Sprintf("%s:%d", conf.Host, conf.Port)))
 }
